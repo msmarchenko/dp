68f6d79ca4b11ccf0ba293700b3c5bf1b1268dc6
MarinaOct <marinaoctober98@gmail.com>
2019-11-02 16:58:08 +0300

    fix charts

--- front/src/components/Charts.vue
+++ front/src/components/Charts.vue
@@ -1,14 +1,51 @@
 <template>
     <div id="chart">
-        <b-container>
+        <b-container class="mt-5">
             <b-row>
-                <b-col>
-                    <apexchart type=scatter   :options="chart1.chartOptions" :series="chart1.series" />
+                <b-col cols="4" >
+                    <b-card-group deck>
+                        <b-card border-variant="info" header="Параметры" align="center">
+                            <b-card-text>
+                                <p>
+                                   Технические Параметр: ExtB_Ist_Massedruck                                  
+                                </p>
+                                <p>
+                                    Показатель качества: Stippe_-3000                                 
+                                </p>
+
+                            </b-card-text>
+                        </b-card>
+                    </b-card-group>
+                    <b-card-group deck class="mt-5">                        
+                        <b-card border-variant="info" header="Информация" align="center">
+                            <b-card-text>
+                                <p>
+                                    Коеф. Лин.Рег.: {{coefc}}                                    
+                                </p>
+                                <p>
+                                    Cреднеквадратическая ошибка: {{mean_squared_error}}                                    
+                                </p>
+                                <p>
+                                    ROC/AUC: {{roc_auc}}                                    
+                                </p>
+                                <p>
+                                    Дисперсия: {{variance_score}}                                    
+                                </p>
+
+                            </b-card-text>
+                        </b-card>
+                        
+                    </b-card-group>
                 </b-col>
-                <b-col>
-                    <apexchart type=bar  :options="chart2.chartOptions" :series="chart2.series" />
+                <b-col cols="8">
+                    <h2>Графический результат</h2>
+                    <apexchart type=line :options="chart1.chartOptions" :series="chart1.series" />
                 </b-col>
+                
             </b-row>
+            <b-col>
+                <!-- <apexchart type=bar  :options="chart2.chartOptions" :series="chart2.series" /> -->
+            </b-col>
         </b-container>
       
       
@@ -24,52 +61,183 @@ export default {
     },
     created(){
         
-        this.dataShow = JSON.parse(this.$store.getters.DATASHOW);        
+        this.dataShow = this.$store.getters.DATASHOW;        
         if(this.dataShow != null)
         {
             this.chart1.series[0].data = []
+            this.chart1.series[1].data = []
+            this.coefc = this.dataShow.coefc[0][0];
+            this.roc_auc = this.dataShow.roc_auc;
+            this.mean_squared_error = this.dataShow.mean_squared_error;
+            this.variance_score = this.dataShow.variance_score;
             var app = this;
             Object.values(this.dataShow.X_test).map(function(value, key) {
-                app.chart1.series[0].data.push([value[0], app.dataShow.y_test[key][0]])
+                // eslint-disable-next-line 
+                
+                app.chart1.series[0].data.push({x:value[0], y:app.dataShow.y_test[key][0]})
             });
-        }
-        console.l
+            Object.values(this.dataShow.y_pred).map(function(value, key) {
+                // eslint-disable-next-line 
+                // console.log([value[0], app.dataShow.y_test[key][0]])    
+                app.chart1.series[1].data.push({x:app.dataShow.X_test[key][0], y:value[0]})
+            });
+        }   
     },
     data() {
         return {
             dataShow: null,
+            coefc: 0.0,
+            roc_auc: 0.0,
+            mean_squared_error: 0.0,
+            variance_score: 0.0,
             chart1: 
             {
                 series: [
                     {
-                    name: 'series1',
-                    data: [100, 0, -100, 100, -100, 0, 100]
+                        name: 'Parametrs',
+                        type: 'scatter',
+                        data: [{ x: 1,
+                            y: 2.14
+                        }, {
+                            x: 1.2,
+                            y: 2.19
+                        }, {
+                            x: 1.8,
+                            y: 2.43
+                        }, {
+                            x: 2.3,
+                            y: 3.8
+                        }, {
+                            x: 2.6,
+                            y: 4.14
+                        }, {
+                            x: 2.9,
+                            y: 5.4
+                        }, {
+                            x: 3.2,
+                            y: 5.8
+                        }, {
+                            x: 3.8,
+                            y: 6.04
+                        }, {
+                            x: 4.55,
+                            y: 6.77
+                        }, {
+                            x: 4.9,
+                            y: 8.1
+                        }, {
+                            x: 5.1,
+                            y: 9.4
+                        }, {
+                            x: 7.1,
+                            y: 7.14
+                        }, {
+                            x: 9.18,
+                            y: 8.4
+                        }]
                     }, 
-                    // {
-                    //     name: 'series2',
-                    //     data: [11, 32, 45, 32, 34, 52, 41]
-                    // }
+                    {
+                        name: 'predict',
+                        type: 'line',
+                        data: [{
+                                    x: 1,
+                                    y: 2
+                                }, {
+                                    x: 2,
+                                    y: 3
+                                }, {
+                                    x: 3,
+                                    y: 4
+                                }, {
+                                    x: 4,
+                                    y: 5
+                                }, {
+                                    x: 5,
+                                    y: 6
+                                }, {
+                                    x: 6,
+                                    y: 7
+                                }, {
+                                    x: 7,
+                                    y: 8
+                                }, {
+                                    x: 8,
+                                    y: 9
+                                }, {
+                                    x: 9,
+                                    y: 10
+                                }, {
+                                    x: 10,
+                                    y: 11
+                                }]
+                    },
+                    {
+                        name: 'real',
+                        type: 'line',
+                        data: [{
+                                    x: 1,
+                                    y: 2
+                                }, {
+                                    x: 2,
+                                    y: 3
+                                }, {
+                                    x: 3,
+                                    y: 4
+                                }, {
+                                    x: 4,
+                                    y: 5
+                                }, {
+                                    x: 5,
+                                    y: 6
+                                }, {
+                                    x: 6,
+                                    y: 7
+                                }, {
+                                    x: 7,
+                                    y: 8
+                                }, {
+                                    x: 8,
+                                    y: 10
+                                }, {
+                                    x: 9,
+                                    y: 12
+                                }, {
+                                    x: 10,
+                                    y: 14
+                                }]
+                    }
                 ],
                 chartOptions: {
-                    chart: {
-                        zoom: {
-                        enabled: true,
-                        type: 'xy'
-                        }
+                    fill: {
+                        type: 'solid',
+                    },
+                    markers: {
+                        size: [6, 0]
+                    },
+                    tooltip: {
+                        shared: false,
+                        intersect: true,
+                    },
+                    legend: {
+                        show: true
                     },
                     xaxis: {
-                        tickAmount: 10,
+                        type: 'numeric',
+                        tickAmount: 12,
                         labels: {
                             formatter: function(val) {
                                 return parseFloat(val).toFixed(1)
                             }
                         }
                     },
-                    yaxis: {
-                        tickAmount: 7
-                    }
-                }      
-
+                    yaxis:{
+                        labels: {
+                            formatter: function(val) {
+                                return parseFloat(val).toFixed(1)
+                            }
+                        }
+                    }   
+                }
             },
             chart2:{
                 series: [{
